7e333175ae17ec65b43e8979e77aefd83d6556a9
Josemar brito <estudos.josemarbrito@gmail.com>
2023-11-29 22:57:02 -0300

    Corrigido falhas com flake8

--- livro/models.py
+++ livro/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from datetime import date
-class Livros (models.Model):
-    nome = models.CharField (max_length=50)
-    autor = models.CharField(max_length=50, blank = True)
-    tema = models.CharField(max_length=50, blank = True)
-    data_cadastro = models.DateField(default = date.today)
-    emprestado = models.BooleanField(default=False)
-    nome_emprestado = models.CharField(max_length=50, blank = True)
-    data_emprestimo = models.DateTimeField(blank = True, null = True)
-    data_devolucao = models.DateTimeField(blank = True, null = True)
-    tempo_duracao = models.DateField(blank = True, null = True)
 
-    
+
+class Livros (models.Model):
+    nome = models.CharField(max_length=50)
+    autor = models.CharField(max_length=50, blank=True)
+    tema = models.CharField(max_length=50, blank=True)
+    data_cadastro = models.DateField(default=date.today)
+    emprestado = models.BooleanField(default=False)
+    nome_emprestado = models.CharField(max_length=50, blank=True)
+    data_emprestimo = models.DateTimeField(blank=True, null=True)
+    data_devolucao = models.DateTimeField(blank=True, null=True)
+    tempo_duracao = models.DateField(blank=True, null=True)
+
     class Meta:
         verbose_name = 'Livro'
-        
